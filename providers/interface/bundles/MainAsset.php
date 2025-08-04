3dd9910ce0cde8422d579272344790fc37584137
Itachi <bscs157j2022@students.tum.ac.ke>
2025-08-05 01:38:52 +0300

    removed jquery from assests using yii's default one

--- providers/interface/bundles/MainAsset.php
+++ providers/interface/bundles/MainAsset.php
@@ -8,17 +8,8 @@ class MainAsset extends AssetBundle
 {
     public $basePath = '@ui/assets';
     public $baseUrl = '@web/providers/interface/assets';
+
     public $css = [
-        [
-            'href' => 'oneui/favicon.png',
-            'rel' => 'icon',
-            'sizes' => '64x64',
-            // 'admin/assets/img/favicon.png',
-
-        ],
-        // 'oneui/css/dashboard.css',
-
-
         'admin/assets/plugins/fontawesome/css/fontawesome.min.css',
         'admin/assets/plugins/fontawesome/css/all.min.css',
         'admin/assets/plugins/feather/feather.css',
@@ -29,54 +20,21 @@ class MainAsset extends AssetBundle
         'admin/assets/css/bootstrap-datetimepicker.min.css',
         'admin/assets/css/owl.carousel.min.css',
         'admin/assets/css/style.css',
-        'admin/custom.css'
+    ];
 
-    ];
     public $js = [
-        [
-            'src' => 'admin/assets/js/jquery-3.7.1.min.js',
-            'type' => 'a6198a1a8153d4da5e6d6c13-text/javascript',
-        ],
-        [
-            'src' => 'admin/assets/js/bootstrap.bundle.min.js',
-            'type' => 'a6198a1a8153d4da5e6d6c13-text/javascript',
-        ],
-        [
-            'src' => 'admin/assets/js/feather.min.js',
-            'type' => 'a6198a1a8153d4da5e6d6c13-text/javascript',
-        ],
-        [
-            'src' => 'admin/assets/plugins/slimscroll/jquery.slimscroll.min.js',
-            'type' => 'a6198a1a8153d4da5e6d6c13-text/javascript',
-        ],
-        [
-            'src' => 'admin/assets/plugins/apexchart/apexcharts.min.js',
-            'type' => 'a6198a1a8153d4da5e6d6c13-text/javascript',
-        ],
-        [
-            'src' => 'admin/assets/plugins/apexchart/chart-data.js',
-            'type' => 'a6198a1a8153d4da5e6d6c13-text/javascript',
-        ],
-        [
-            'src' => 'admin/assets/js/theme-settings.js',
-            'type' => 'a6198a1a8153d4da5e6d6c13-text/javascript',
-        ],
-        [
-            'src' => 'admin/assets/js/greedynav.js',
-            'type' => 'a6198a1a8153d4da5e6d6c13-text/javascript',
-        ],
-        [
-            'src' => 'admin/assets/js/script.js',
-            'type' => 'a6198a1a8153d4da5e6d6c13-text/javascript',
-        ],
-        [
-            'src' => '../../cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js',
-            'data-cf-settings' => 'a6198a1a8153d4da5e6d6c13-|49',
-            'defer' => true,
-        ],
+        // 'admin/assets/js/jquery-3.7.1.min.js',
+        'admin/assets/js/bootstrap.bundle.min.js',
+        'admin/assets/js/feather.min.js',
+        'admin/assets/plugins/slimscroll/jquery.slimscroll.min.js',
+        'admin/assets/plugins/apexchart/apexcharts.min.js',
+        'admin/assets/plugins/apexchart/chart-data.js',
+        'admin/assets/js/theme-settings.js',
+        'admin/assets/js/greedynav.js',
+        'admin/assets/js/script.js',
     ];
+
     public $depends = [
-        'helpers\widgets\swal\AlertAsset',
         'yii\web\YiiAsset',
     ];
 }
