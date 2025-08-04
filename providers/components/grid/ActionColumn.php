65b446e209a538b8258b124db2372e9fd0bcd4ad
Itachi <bscs157j2022@students.tum.ac.ke>
2025-08-05 01:30:16 +0300

    added a custom class and styling for the action column

--- providers/components/grid/ActionColumn.php
+++ providers/components/grid/ActionColumn.php
@@ -4,4 +4,12 @@ namespace helpers\grid;
 
 class ActionColumn extends \yii\grid\ActionColumn
 {
+    public function init()
+    {
+        $this->contentOptions = [
+            'style' => 'display: flex; align-items: center; justify-content: center; gap: 2px;',
+            'class' => 'action-column',
+        ];
+        parent::init(); // Don't forget this!
+    }
 }
