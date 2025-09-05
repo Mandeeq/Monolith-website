b5423bebf49554b9d729f45a1c084c54c9efca65
codetheuri <theurij113@gmail.com>
2025-09-05 23:49:14 +0300

    icons working

--- providers/interface/bundles/MainAsset.php
+++ providers/interface/bundles/MainAsset.php
@@ -24,6 +24,8 @@ class MainAsset extends AssetBundle
         'css/nice-select.css',
         'css/all.css',
         'css/style.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+        'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap',
     ];
 
     public $js = [
