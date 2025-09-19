3b0083173190a20db6027c61d9a77576c7d9cb44
codetheuri <theurij113@gmail.com>
2025-09-19 16:57:18 +0300

    minor changes

--- config/web.php
+++ config/web.php
@@ -99,7 +99,6 @@ $config = [
             'enableStrictParsing' => false,
             'showScriptName' => false,
             'rules' => [
-<<<<<<< HEAD
                 // $_ENV['APP_VERSION'] . '/about' => 'site/about',
                 'qaffee/' => 'qaffee/about-sections/index',
                 '/' => 'site/index',
@@ -107,13 +106,6 @@ $config = [
                 '/contact' => 'site/contact',
                 '/menu'=>'site/menu',
                 '/blog'=>'site/blog',
-=======
-                $_ENV['APP_VERSION'] . '/about' => 'site/about',
-                'qaffee/' => 'qaffee/about-sections/index',
-<<<<<<< HEAD
->>>>>>> 61d1703ca919348c5a08ecb032abc770f0c168df
-=======
->>>>>>> 61d1703ca919348c5a08ecb032abc770f0c168df
                 [
                     'pattern' => $_ENV['APP_VERSION'] . '/docs/openapi-json-resource',
                     'route' => 'site/json-docs',
