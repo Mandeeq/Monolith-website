c3df0340817888a85efe48b2fde35873ac2b48ee
Mandeeq <imanjamal370@gmail.com>
2025-09-20 11:09:45 +0300

    banners

--- providers/interface/views/components/qaffeehome/banner.php
+++ providers/interface/views/components/qaffeehome/banner.php
@@ -1,33 +1,26 @@
 <?php
 use yii\helpers\Html;
 ?>
-<section class="" style="position: relative; overflow: hidden;">
-  
-  <!-- Banner Image -->
-   <?= Html::img('@web/web/assets/img/banner_bg.png', [
-                'alt' => 'banner_bg ',
-                // 'class' => 'img-fluid',
-                'style' => '
-            width:100%; height:100%; object-fit: cover; position: absolute; top:0; left:0; z-index:-1;
-             
-             ', // adjust size here
-            ]) ?>
-  <img src="/e-comerce/e-commerce-app/web/assets/img/banner_bg.png" 
-       alt="Banner Background" 
-       style="width:100%; height:100%; object-fit: cover; position: absolute; top:0; left:0; z-index:-1;">
-
-  <div class="container">
-    <div class="row align-items-center">
-      <div class="col-lg-7">
-        <div class="banner_text">
-          <div class="banner_text_iner">
-            <h5>Expensive but the best</h5>
-            <h1>Modern restaurant in center of the city</h1>
-            <a href="#" class="btn_1">book a table</a>
-          </div>
+<section style="overflow: hidden;">
+    <div class="banner" style="
+        background-image: url('<?= Yii::getAlias('@web') ?>/web/assets/img/banner_bg.png');
+        background-size: cover;
+        background-position: center;
+        height: 1080px;
+        position: relative;
+    ">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-lg-7">
+                    <div class="banner_text">
+                        <div class="banner_text_iner">
+                            <h5 style="color: white;">Expensive but the best</h5>
+                            <h1 style="color: white;">Modern restaurant in center of the city</h1>
+                            <a href="#" class="btn_1">book a table</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-
 </section>
